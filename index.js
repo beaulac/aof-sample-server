3558652394e6a164e4001634a3395c6efcac5d3a
Antoine Beauvais-Lacasse <antoine.beaulac@gmail.com>
2019-03-12 22:02:03 -0400

    Switch to now.sh

--- index.js
+++ index.js
@@ -1,4 +1,54 @@
 'use strict';
+const request = require('request');
 
-require('./cors.server');
-require('./samples.cache.server');
+const driveFilesEndpoint = 'https://content.googleapis.com/drive/v3/files';
+
+const apiKey = 'AIzaSyB34kcbBYm9f0lpjwdm1_RDubFRZPwi0pA';
+const folderId = '0B721XY-cG39uX3M3eFk3Y0dKRms';
+
+// Super naive memory cache.
+const sampleFiles = [];
+
+function getSamplesAtPage(pageToken) {
+    return new Promise(
+        (resolve, reject) => {
+            return request.get({
+                    url: driveFilesEndpoint,
+                    qs: { q: `'${ folderId }' in parents`, key: apiKey, pageToken },
+                    json: true
+                },
+                (err, res, body) => err || !res || !body
+                    ? reject(err || new Error('NORES'))
+                    : resolve(body)
+            );
+        }
+    );
+}
+
+/**
+ * @param nextPageToken
+ * @returns {Promise<Array>}
+ */
+function getSampleFiles(nextPageToken) {
+    let pageToken;
+    return getSamplesAtPage(nextPageToken)
+        .then(response => {
+            const { files, nextPageToken } = response;
+
+            pageToken = nextPageToken;
+            sampleFiles.push(...files);
+            return nextPageToken ? getSampleFiles(pageToken) : sampleFiles;
+        });
+}
+
+module.exports = async (req, res) => {
+    try {
+        const samples = await getSampleFiles();
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(samples));
+    } catch (err) {
+        res.statusCode = 503;
+        res.end();
+    }
+};
--- origin.whitelist.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = [
-    'http://localhost:4200',
-    'http://bdtem.co.in',
-    'http://www.bdtem.co.in',
-    'http://bdtem.com',
-    'http://www.bdtem.com'
-];
